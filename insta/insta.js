fc170bbd4867307257f1de2012d556a151cd263f
mohammadafnan <mohammadafnan29>
2024-08-14 13:17:14 -0700

    instagram clone main page 14 aug 2014

---
+++ insta/insta.js
@@ -0,0 +1,92 @@
+
+function save() {
+    // alert("works")
+    var save = document.getElementById("save");
+    var fillsave = document.getElementById("fillsave");
+    save.style.display="none";
+    fillsave.style.display="block";
+
+    // save.style.visibility="visible";
+    // fillsave.style.visibility="visible";
+    // if (fillsave.style.visibility == "hidden" || save.style.visibility ) {
+    //     fillsave.style.visibility = "visible";
+    //     save.style.visibility = "visible";
+
+    // } else {
+    //     fillsave.style.visibility = "visible";
+    //     save.style.visibility = "hidden";
+
+
+    // }
+
+}
+
+
+function save2() {
+    // alert("works")
+    var save = document.getElementById("save");
+    var fillsave = document.getElementById("fillsave");
+    save.style.display="block";
+    fillsave.style.display="none";
+
+    // save.style.visibility="visible";
+    // fillsave.style.visibility="visible";
+    // if (fillsave.style.visibility == "hidden" || save.style.visibility ) {
+    //     fillsave.style.visibility = "visible";
+    //     save.style.visibility = "visible";
+
+    // } else {
+    //     fillsave.style.visibility = "visible";
+    //     save.style.visibility = "hidden";
+
+
+    // }
+
+}
+
+
+
+function heartsave() {
+    // alert("works")
+    var saveheart1 = document.getElementById("saveheart1");
+    var saveheart2 = document.getElementById("saveheart2");
+    saveheart1.style.display="none";
+    saveheart2.style.display="flex";
+
+    // save.style.visibility="visible";
+    // fillsave.style.visibility="visible";
+    // if (fillsave.style.visibility == "hidden" || save.style.visibility ) {
+    //     fillsave.style.visibility = "visible";
+    //     save.style.visibility = "visible";
+
+    // } else {
+    //     fillsave.style.visibility = "visible";
+    //     save.style.visibility = "hidden";
+
+
+    // }
+
+}
+
+
+function heartsave2() {
+    // alert("works")
+    var saveheart1 = document.getElementById("saveheart1");
+    var saveheart2 = document.getElementById("saveheart2");
+    saveheart1.style.display="block";
+    saveheart2.style.display="none";
+
+    // save.style.visibility="visible";
+    // fillsave.style.visibility="visible";
+    // if (fillsave.style.visibility == "hidden" || save.style.visibility ) {
+    //     fillsave.style.visibility = "visible";
+    //     save.style.visibility = "visible";
+
+    // } else {
+    //     fillsave.style.visibility = "visible";
+    //     save.style.visibility = "hidden";
+
+
+    // }
+
+}
